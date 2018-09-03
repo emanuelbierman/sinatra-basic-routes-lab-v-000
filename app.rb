e190270944809bce021dc92e62e2524855738c66
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-09-03 16:49:45 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,9 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  # The `name` route should display "My name is \_\_" in the browser, the `hometown` route should display "My hometown is \_\_", and the `favorite-song` route should display "My favorite song is \_\_".
+
+  
+  
 end
